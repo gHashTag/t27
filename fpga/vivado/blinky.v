a513183858a79dc953d4d73b414121f4e2aeb2ec
Dmitriy Vasilev <raoffonom@icloud.com>
2026-05-08 11:20:42 +0700

    fix(fpga): replicate exact QMTECH ref design with reset H7 + LEDs G20/G21/R23/T23 (Closes #305)

--- fpga/vivado/blinky.v
+++ fpga/vivado/blinky.v
@@ -1,16 +1,36 @@
 module blinky (
-    input  wire clk,
-    output wire led5,
-    output wire led6
+    input sys_clk,
+    input sys_rst_n,
+    output led_g20,
+    output led_g21,
+    output led_r23,
+    output led_t23
 );
 
-    reg [26:0] counter = 0;
+reg [31:0] count;
+reg r_led;
 
-    always @(posedge clk) begin
-        counter <= counter + 1;
-    end
+always @(posedge sys_clk or negedge sys_rst_n) begin
+    if (!sys_rst_n)
+        count <= 32'd0;
+    else if (count == 32'd50_000_000)
+        count <= 32'd0;
+    else
+        count <= count + 32'd1;
+end
 
-    assign led5 = ~counter[23];
-    assign led6 = ~counter[22];
+always @(posedge sys_clk or negedge sys_rst_n) begin
+    if (!sys_rst_n)
+        r_led <= 1'b0;
+    else if (count < 32'd25_000_000)
+        r_led <= 1'b1;
+    else
+        r_led <= 1'b0;
+end
+
+assign led_g20 = r_led;
+assign led_g21 = ~r_led;
+assign led_r23 = r_led;
+assign led_t23 = ~r_led;
 
 endmodule
